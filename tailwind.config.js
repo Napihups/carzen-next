80910c3a684b867be5b4cf5d10ed73cb6712fb48
Hanafi <programminghup@gmail.com>
2022-04-27 01:41:54 +0800

    setup nextjs project with tailwind configured

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      gray: "#667085",
+      primary: "#038C8C",
+      white: "#ffffff",
+      black: "#eeeeee",
+    },
+    borderRadius: {
+      none: "0",
+      DEFAULT: "3px",
+      full: "9999px",
+    },
+  },
+  plugins: [],
+};
