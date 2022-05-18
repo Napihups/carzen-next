79d028a6aa3616d8582177d09ac9a99227a963cc
Hanafi <programminghup@gmail.com>
2022-05-18 15:52:48 +0800

    Completed Refactoring CzNavbar

--- app/components/modules/CzNavbar/CzNavMenu.tsx
+++ app/components/modules/CzNavbar/CzNavMenu.tsx
@@ -1,5 +1,65 @@
+import { fixtures } from "@constant/fixtures";
+import { pathNames } from "@constant/router-config";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
+import { CzNavMenuExtDropdown } from "./CzNavMenuExtDropdown";
+
+const _setItemActiveClass = (currentPath: string, itemPath: string) => {
+  return currentPath === itemPath ? "active" : "";
+};
 
 export const CzNavMenu: React.FC = () => {
-  return <div className="czNavMenu"></div>;
+  const router = useRouter();
+
+  return (
+    <ul className="czNavMenu">
+      <Link passHref href={pathNames.new_cars}>
+        <li className={`czNavMenu__menuItem ${_setItemActiveClass(router.pathname, pathNames.new_cars)}`}>
+          {fixtures.nav_menu.new_cars}
+        </li>
+      </Link>
+      <Link passHref href={pathNames.used_cars}>
+        <li className={`czNavMenu__menuItem ${_setItemActiveClass(router.pathname, pathNames.used_cars)}`}>
+          {fixtures.nav_menu.used_cars}
+        </li>
+      </Link>
+      <Link passHref href={pathNames.rental_cars}>
+        <li className={`czNavMenu__menuItem ${_setItemActiveClass(router.pathname, pathNames.rental_cars)}`}>
+          {fixtures.nav_menu.rental_cars}
+        </li>
+      </Link>
+
+      <Link passHref href={pathNames.sell_my_car}>
+        <li className={`czNavMenu__menuItem ${_setItemActiveClass(router.pathname, pathNames.sell_my_car)}`}>
+          {fixtures.nav_menu.sell_my_car}
+        </li>
+      </Link>
+
+      <div className="czNavMenu__item-wrapper">
+        <Link passHref href={pathNames.directory}>
+          <li className={`czNavMenu__menuItem ${_setItemActiveClass(router.pathname, pathNames.directory)}`}>
+            {fixtures.nav_menu.directory}
+          </li>
+        </Link>
+        <Link passHref href={pathNames.product}>
+          <li className={`czNavMenu__menuItem ${_setItemActiveClass(router.pathname, pathNames.product)}`}>
+            {fixtures.nav_menu.product}
+          </li>
+        </Link>
+        <Link passHref href={pathNames.car_videos}>
+          <li className={`czNavMenu__menuItem ${_setItemActiveClass(router.pathname, pathNames.car_videos)}`}>
+            {fixtures.nav_menu.car_videos}
+          </li>
+        </Link>
+        <Link passHref href={pathNames.articles}>
+          <li className={`czNavMenu__menuItem ${_setItemActiveClass(router.pathname, pathNames.articles)}`}>
+            {fixtures.nav_menu.articles}
+          </li>
+        </Link>
+      </div>
+      <CzNavMenuExtDropdown />
+      {/* <NavMenuExtendedDropdown /> */}
+    </ul>
+  );
 };
---
+++ app/components/modules/CzNavbar/CzNavMenuExtDropdown.tsx
@@ -0,0 +1,66 @@
+import { fixtures } from "@constant/fixtures";
+import { pathNames } from "@constant/router-config";
+import { Menu, Transition } from "@headlessui/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { Fragment } from "react";
+import { IoEllipsisHorizontal } from "react-icons/io5";
+
+export const CzNavMenuExtDropdown: React.FC = () => {
+  const router = useRouter();
+
+  return (
+    <Menu as="div" className="czNavMenuExtDropdown">
+      <div>
+        <Menu.Button className="czNavMenu__menuItem dropdown-toggler">
+          <IoEllipsisHorizontal size="20px" />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="czNavMenuExtDropdown__items">
+          <Menu.Item>
+            {({ active }) => (
+              <Link passHref href={pathNames.directory}>
+                <li className={`czNavMenuExtDropdown__item ${active ? "active" : ""}`}>
+                  {fixtures.nav_menu.directory}
+                </li>
+              </Link>
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <Link passHref href={pathNames.product}>
+                <li className={`czNavMenuExtDropdown__item ${active ? "active" : ""}`}>{fixtures.nav_menu.product}</li>
+              </Link>
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <Link passHref href={pathNames.car_videos}>
+                <li className={`czNavMenuExtDropdown__item ${active ? "active" : ""}`}>
+                  {fixtures.nav_menu.car_videos}
+                </li>
+              </Link>
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <Link passHref href={pathNames.articles}>
+                <li className={`czNavMenuExtDropdown__item ${active ? "active" : ""}`}>{fixtures.nav_menu.articles}</li>
+              </Link>
+            )}
+          </Menu.Item>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+};
